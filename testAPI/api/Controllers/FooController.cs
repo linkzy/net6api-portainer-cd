d1d4111986565d4d76adf6dfd95e69c1e24590dc
LinKzy <danilo.ut@gmail.com>
2022-06-08 12:47:09 -0300

    add new controller

---
+++ testAPI/api/Controllers/FooController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooController : ControllerBase
+    {
+        // GET: api/<FooController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "bar" };
+        }
+    }
+}
